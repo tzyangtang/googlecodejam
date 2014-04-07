000213bfcfc54ba7e3423f9441d3dcaef0f3dc00
tzyangtang <tzyangtang@gmail.com>
2014-04-07 14:55:34 -0400

    2008 qualification round - welcome to code jam

--- src/com/tzyangtang/googlecodejam/practice/welcometocodejam/WelcomeToCodeJam.java
+++ src/com/tzyangtang/googlecodejam/practice/welcometocodejam/WelcomeToCodeJam.java
@@ -6,8 +6,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class WelcomeToCodeJam {
+
+//	static HashMap<String, Integer> pastResults = new HashMap<>();
 
 	/**
 	 * @param args
@@ -28,7 +31,7 @@ public class WelcomeToCodeJam {
 			for (int i = 0; i < numberOfCases; i++) {
 				String testString = br.readLine();
 
-				int count = howManyTimesItAppear(testString, welcomeToCodeJam);
+				int count = solve(testString, welcomeToCodeJam);
 				String outputCount = String.format("%04d", count);
 
 				String msg = "Case #" + (i + 1) + ": " + outputCount + "\n";
@@ -41,47 +44,79 @@ public class WelcomeToCodeJam {
 		}
 	}
 
-	private static int howManyTimesItAppear(String testString,
-			String welcomeToCodeJam) {
+	private static int solve(String target, String patternString) {
+		char[] pattern = patternString.toCharArray();
+		int patternLength = pattern.length;
+		int MODULO = 10000;
 
-		if (testString.length() == 0 || welcomeToCodeJam.length() == 0) {
-			return 0;
-		}
+		char[] line = target.toCharArray();
+		int[] result = new int[patternLength + 1];
+		result[0] = 1;
 
-		if (welcomeToCodeJam.length() == 1) {
-			int subCount = 0;
-			for (int i = 0; i < testString.length(); i++) {
-				if (welcomeToCodeJam.charAt(0) == testString.charAt(i))
-					subCount++;
-			}
-			return subCount;
-		}
-
-		if (testString.length() == 1) {
-			if (welcomeToCodeJam.length() == 1) {
-				if (testString.charAt(0) == welcomeToCodeJam.charAt(0)) {
-					return 1;
-				} else {
-					return 0;
+		for (char c : line) {
+			for (int i = patternLength - 1; i >= 0; i--) {
+				if (c == pattern[i]) {
+					result[i + 1] += result[i];
+					result[i + 1] %= MODULO;
 				}
-			} else {
-				return 0;
 			}
 		}
 
-		if (testString.charAt(testString.length() - 1) == welcomeToCodeJam
-				.charAt(welcomeToCodeJam.length() - 1)) {
-			return howManyTimesItAppear(
-					testString.substring(0, testString.length() - 1),
-					welcomeToCodeJam)
-					+ howManyTimesItAppear(
-							testString.substring(0, testString.length() - 1),
-							welcomeToCodeJam.substring(0,
-									welcomeToCodeJam.length() - 1));
-		} else {
-			return howManyTimesItAppear(
-					testString.substring(0, testString.length() - 1),
-					welcomeToCodeJam);
-		}
+		return result[patternLength];
 	}
+//
+//	private static int howManyTimesItAppear(String testString,
+//			String welcomeToCodeJam) {
+//
+//		String key = testString + "|" + welcomeToCodeJam;
+//		if (pastResults.containsKey(key)) {
+//			System.out.print("matched key:" + key + "\n");
+//			return pastResults.get(key);
+//		}
+//
+//		if (testString.length() == 0 || welcomeToCodeJam.length() == 0) {
+//			pastResults.put(key, 0);
+//			return 0;
+//		}
+//
+//		if (welcomeToCodeJam.length() == 1) {
+//			int subCount = 0;
+//			for (int i = 0; i < testString.length(); i++) {
+//				if (welcomeToCodeJam.charAt(0) == testString.charAt(i))
+//					subCount++;
+//			}
+//			pastResults.put(key, subCount);
+//			return subCount;
+//		}
+//
+//		if (testString.length() == 1) {
+//			if (welcomeToCodeJam.length() == 1) {
+//				if (testString.charAt(0) == welcomeToCodeJam.charAt(0)) {
+//					pastResults.put(key, 1);
+//					return 1;
+//				} else {
+//					pastResults.put(key, 0);
+//					return 0;
+//				}
+//			} else {
+//				pastResults.put(key, 0);
+//				return 0;
+//			}
+//		}
+//
+//		if (testString.charAt(testString.length() - 1) == welcomeToCodeJam
+//				.charAt(welcomeToCodeJam.length() - 1)) {
+//			return howManyTimesItAppear(
+//					testString.substring(0, testString.length() - 1),
+//					welcomeToCodeJam)
+//					+ howManyTimesItAppear(
+//							testString.substring(0, testString.length() - 1),
+//							welcomeToCodeJam.substring(0,
+//									welcomeToCodeJam.length() - 1));
+//		} else {
+//			return howManyTimesItAppear(
+//					testString.substring(0, testString.length() - 1),
+//					welcomeToCodeJam);
+//		}
+//	}
 }
